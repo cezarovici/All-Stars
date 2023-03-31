a59dafc0809d98346caeec201b03d5bdeb7223b4
Apetroaei Cezar <njroffic@gmail.com>
2023-03-31 18:04:07 +0300

    adding game objects members and methods

--- src/PaooGame/GameObjects/GameObject.java
+++ src/PaooGame/GameObjects/GameObject.java
@@ -1,4 +1,27 @@
 package PaooGame.GameObjects;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class GameObject {
+    private BufferedImage sprite;
+    private int x , y;
+
+    protected void Draw(Graphics graphics){
+        graphics.drawImage(sprite,x,y,null);
+    }
+
+    protected void moveY(int y){
+        this.y += y;
+    }
+
+    protected void moveX(int x){
+        this.x += x;
+    }
+
+    protected void move(int x, int y){
+        moveY(y);
+        moveX(x);
+    }
+
 }
