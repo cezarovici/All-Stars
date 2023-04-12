1849bbf24dd2c563ece86c7eadef9bb1e6e42c9d
Apetroaei Cezar <njroffic@gmail.com>
2023-04-12 08:50:47 +0300

    add  keyboards inputs

---
+++ src/PaooGame/UserInterface/Keyboard.java
@@ -0,0 +1,27 @@
+package PaooGame.UserInterface;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Keyboard {
+    // Based on https://github.com/RealKC/WizardGame2/blob/master/src/WizardGame2/Keyboard.java
+
+    private static final Map<Integer, Boolean> pressedKeys = new HashMap<>();
+
+    static {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(event -> {
+            synchronized (Keyboard.class) {
+                if (event.getID() == KeyEvent.KEY_PRESSED) pressedKeys.put(event.getKeyCode(), true);
+                else if (event.getID() == KeyEvent.KEY_RELEASED) pressedKeys.put(event.getKeyCode(), false);
+                return false;
+            }
+        });
+    }
+
+
+    public static boolean isKeyPressed(int keyCode) { // Any key code from the KeyEvent class
+        return pressedKeys.getOrDefault(keyCode, false);
+    }
+}
