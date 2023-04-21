714eb2eaf7d57ca81edc547999d308019771a7d3
Apetroaei Cezar <njroffic@gmail.com>
2023-04-22 01:56:32 +0300

    basket class added and implemented left and right basket

--- src/PaooGame/GameObjects/Basket.java
+++ src/PaooGame/GameObjects/Basket.java
@@ -1,4 +1,17 @@
 package PaooGame.GameObjects;
 
-public class Basket {
+import PaooGame.GameObjects.GameObject;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Basket extends GameObject {
+
+    public Basket(BufferedImage sprite, int x, int y) {
+        super(sprite, x, y);
+    }
+
+    public void Draw(Graphics g){
+        super.Draw(g);
+    }
 }
